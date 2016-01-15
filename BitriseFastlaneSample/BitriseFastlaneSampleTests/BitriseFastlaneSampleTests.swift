0de379402d4ce564d5e541dde1df8dbe42594d2e
Viktor Benei <viktor.benei@gmail.com>
2016-01-15 14:51:20 +0100

    initial

---
+++ BitriseFastlaneSample/BitriseFastlaneSampleTests/BitriseFastlaneSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitriseFastlaneSampleTests.swift
+//  BitriseFastlaneSampleTests
+//
+//  Created by Viktor Benei on 15/01/16.
+//  Copyright © 2016 bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import BitriseFastlaneSample
+
+class BitriseFastlaneSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
